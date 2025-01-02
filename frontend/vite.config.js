a94aaeb2c4a3c45352746be34eb09d29fa5115a1
Bablu kumar <bk559722@gmail.com>
2025-01-02 17:17:59 +0530

    change scripts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,14 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://shopkart-sarq.onrender.com/', // Your backend server URL
-        changeOrigin: true,
-
+        changeOrigin: true, // Ensures the origin of the host header matches the target
+        secure: false, // Allows proxying to servers with self-signed certificates
       },
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['redux'], // Externalize redux to avoid import resolution issues
     },
   },
 });
